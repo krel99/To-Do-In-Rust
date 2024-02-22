bfc4f9ce7bd9405fbb30eab83e81fc478468e612
orcchieftain <karlos.slaj@gmail.com>
2024-02-22 19:53:04 +0100

    Minor update UI

---
+++ src/views/app/items.rs
@@ -0,0 +1,7 @@
+use actix_web::HttpResponse;
+
+pub async fn items() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body("<h1>Items</h1>")
+}
---
+++ src/views/app/mod.rs
@@ -0,0 +1,6 @@
+use actix_web::web;
+mod items;
+
+pub fn app_views_factory(app: &mut web::ServiceConfig) {
+    app.route("/", web::get().to(items::items));
+}
