352d3b8e6e36cdd7fa540da5994094ffb73b22bd
orcchieftain <karlos.slaj@gmail.com>
2024-02-23 14:23:18 +0100

    Create returns HttpResponse instead of string

--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -1,15 +1,18 @@
 use actix_web::HttpRequest;
-use serde_json::value::Value;
+use actix_web::HttpResponse;
+// use serde_json::value::Value;
 use serde_json::Map;
+use serde_json::Value;
 
+use crate::json_serialization::to_do_items::ToDoItems;
 use crate::processes::process_input;
 use crate::state::read_file;
 use crate::to_do::{enums::TaskStatus, to_do_factory};
 
-pub async fn create(req: HttpRequest) -> String {
+pub async fn create(req: HttpRequest) -> HttpResponse {
     let state: Map<String, Value> = read_file("./state.json"); // define what type we are reading from json
     let title: String = req.match_info().get("title").unwrap().to_string(); // get the title from the URL (request)
     let item = to_do_factory(&title.as_str(), TaskStatus::PENDING); // create a new to_do item
     process_input(item, "create".to_string(), &state);
-    return format!("{} created", title); // return a message
+    return HttpResponse::Ok().json(ToDoItems::get_state());
 }
