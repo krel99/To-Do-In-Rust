25c008501e4e578ddee769ed4c7be155f1637ebe
orcchieftain <karlos.slaj@gmail.com>
2024-03-04 16:17:40 +0100

    Updated Item structs

--- src/models/item/item.rs
+++ src/models/item/item.rs
@@ -1,11 +1,15 @@
+use super::super::user::user::User;
 use crate::schema::to_do;
 use chrono::NaiveDateTime;
 
-#[derive(Queryable, Identifiable)]
+// made available by Associations
+#[derive(Queryable, Identifiable, Associations)]
+#[belongs_to(User)]
 #[table_name = "to_do"]
 pub struct Item {
     pub id: i32,
     pub title: String,
     pub status: String,
     pub date: NaiveDateTime,
+    pub user_id: i32,
 }
--- src/models/item/new_item.rs
+++ src/models/item/new_item.rs
@@ -7,15 +7,17 @@ pub struct NewItem {
     pub title: String,
     pub status: String,
     pub date: NaiveDateTime,
+    pub user_id: i32,
 }
 
 impl NewItem {
-    pub fn new(title: String) -> NewItem {
+    pub fn new(title: String, user_id: i32) -> NewItem {
         let now = Utc::now().naive_local();
         return NewItem {
             title,
             status: String::from("PENDING"),
             date: now,
+            user_id,
         };
     }
 }
