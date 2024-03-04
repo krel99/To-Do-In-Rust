e6ee49e7adf3812c126441fee85ba778b96c9da6
orcchieftain <karlos.slaj@gmail.com>
2024-03-04 16:11:47 +0100

    Added User Structs

---
+++ src/models/user/new_user.rs
@@ -0,0 +1,28 @@
+use bcrypt::{hash, DEFAULT_COST};
+use diesel::Insertable;
+use uuid::Uuid;
+
+use crate::schema::users;
+
+#[derive(Insertable, Clone)]
+#[table_name = "users"]
+pub struct NewUser {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+    pub unique_id: String,
+}
+
+impl NewUser {
+    pub fn new(username: String, email: String, password: String) -> NewUser {
+        let hashed_password: String = hash(password.as_str(), DEFAULT_COST).unwrap();
+
+        let uuid = Uuid::new_v4().to_string();
+        return NewUser {
+            username,
+            email,
+            password: hashed_password,
+            unique_id: uuid,
+        };
+    }
+}
